95ec6561c59d5337866bb319cfcd3c2bca4b3f17
calsmith <calsmith@umich.edu>
2022-10-25 23:42:47 -0400

    updated thinking to an easy

--- leetcode/268_easy_missing-number.cpp
+++ leetcode/268_easy_missing-number.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-class Solution{
+class WorseSolution{
 public:
   int missingNumber(vector<int>& nums){
     vector<bool> have_found(nums.size()+1, false);
@@ -19,4 +19,19 @@ public:
     }
     return __INT_MAX__; // this should never happen
   }
+};
+
+// A better solution with O(1) extra space
+// accepted also
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        //find what the sum of numbers 0 to nums.size() should be
+        int sum = (nums.size() * (nums.size() + 1)) / 2;
+        // by subtracting all contained numbers, we find the one that is missing
+        for(auto it = nums.begin(); it != nums.end(); ++it){
+            sum -= *it;
+        }
+        return sum;
+    }
 };
