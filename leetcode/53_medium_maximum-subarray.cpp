4500aea39c90fb3c46b3fb2444023efd79569d7f
calsmith <calsmith@umich.edu>
2022-10-27 15:35:40 -0400

    optimized to do it in O(1) memory

--- leetcode/53_medium_maximum-subarray.cpp
+++ leetcode/53_medium_maximum-subarray.cpp
@@ -1,18 +1,17 @@
 //https://leetcode.com/problems/maximum-subarray/
-// passed
+// passed, 90% memory 80% performance 
 
 
 class Solution {
 	public:
 	int maxSubArray(vector<int>& nums) {
-		vector<int> dp(nums.size());
-		dp[0] = nums[0];
+		int last = nums[0];
 		int max_v = nums[0];
 		for(int i = 1; i < nums.size(); ++i){
 			//find the maximum subarray that ENDS at i
-			dp[i] = max(dp[i-1] + nums[i], nums[i]);
-			if(dp[i] > max_v){
-				max_v = dp[i];
+			last = max(last + nums[i], nums[i]);
+			if(last > max_v){
+				max_v = last;
 			}
 		}
 		return max_v;        
