5265d1ba89d95bfb065c877aa89559ab462834cd
calsmith <calsmith@umich.edu>
2022-11-04 15:21:20 -0400

    p3 redid it to be faster

--- leetcode/3_medium_longest-substring-without-repeating-characters.cpp
+++ leetcode/3_medium_longest-substring-without-repeating-characters.cpp
@@ -3,7 +3,35 @@
 using namespace std;
 //Given a string s, find the length of the longest substring without repeating characters.
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+//accepted
+//99% speed, 91% memory
 
+//kinda sliding window, kinda two pointer. O(n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int cache[256]; //a char has a max value between 0 and 255
+        for(int i = 0; i < 256; ++i)
+            cache[i] = -1;
+        int max_v = 0;
+        //sliding window
+        for(int fast = 0, slow = 0; fast < s.size(); ++fast){
+            if(cache[s[fast]] >= slow){
+                // the character at s[fast] has appeared after the slow pointer. 
+                //[fast, slow] is not a valid window
+                slow = cache[s[fast]] + 1;
+            }
+            int current_size = fast - slow + 1;
+            if(current_size > max_v){
+                max_v = current_size;
+            }
+            cache[s[fast]] = fast;
+        }
+        return max_v;
+    }
+};
+
+//slower, original, more intuitive solution
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -20,7 +48,6 @@ public:
         return maxLength;
     }
 };
-
 
 int main(){
   Solution s;
